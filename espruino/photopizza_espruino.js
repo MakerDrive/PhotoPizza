a0b5bb397c80901012f4031c9adf8f6014541029
MakerDrive <ziqcg.ru@gmail.com>
2017-09-04 01:21:59 +0300

    Added information about the author

--- espruino/photopizza_espruino.js
+++ espruino/photopizza_espruino.js
@@ -1,3 +1,31 @@
+/**
+ * File: photopizza_espruino.js
+ * Created on: August 30, 2017
+ * Description:
+ * PhotoPizza DIY is an open source project of 360° product photography turntable.
+ *
+ * Author: Vladimir Matiyasevith <vladimir.m@makerdrive.ru>
+ * Project Site: PhotoPizza.org
+ *
+ * Copyright: 2017 MakerDrive
+ * Copying permission statement:
+ *  This file is part of PhotoPizza DIY.
+ *
+ *  PhotoPizza DIY is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 this.ShowVersion = 'PhotoPizza v3.1';
 
 this.f = new (require("FlashEEPROM"))();
