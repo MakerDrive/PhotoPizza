a6f6ebeaef7a3d9ed02dd58c6421170b0bc1336d
MakerDrive <vladimir.m@makerdrive.ru>
2019-10-02 14:57:47 +0300

    New firmware version
    + work in the mode of the Wi-Fi access point.
    + connection to the Wi-Fi access point.
    + engine lock in standby mode. It is turned on by a button on the remote control.
    + When you press a button on the remote control, a test shot is taken.
    
    
    Bug fixes.

---
+++ Firmware_for_D340_D480_D700/v6 Espruino ESP32/photopizza_esp32_save_config.js
@@ -0,0 +1,64 @@
+/**
+ * File: photopizza_espruino.js
+ * Created on: August 30, 2017
+ * Description:
+ * PhotoPizza DIY is an open source project of 360° product photography turntable.
+ *
+ * Author: Vladimir Matiyasevith <vladimir.m@makerdrive.ru>
+ * Project Site: PhotoPizza.org
+ *
+ * Copyright: 2018 Vladimir Matiyasevith
+ * Copying permission statement:
+ *  This file is part of PhotoPizza DIY.
+ *
+ *  PhotoPizza DIY is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+fs = require("fs");
+
+var config = {
+  firmwareVersion: 'PhotoPizza v6',
+  wifiSsid: 'PhotoPizza',
+  wifiPassword: '9994501234',
+  wsPort: 8000,
+  state: 'waiting',//started
+  framesLeft: 36,
+  frame: 36,
+  allSteps: 109000,
+  pause: 100,
+  delay: 300,
+  speed: 5000,
+  acceleration: 100,
+  shootingMode: 'inter',
+  direction: 1
+};
+
+
+
+function saveConfig() {
+  fs.writeFileSync('config.txt', JSON.stringify(config));
+  console.log('Save config');
+}
+
+
+E.flashFatFS({ format: true });
+saveConfig();
+
+
+
+var riadTimer = setTimeout(function () {
+  var readConfig = JSON.parse(fs.readFileSync("config.txt"));
+  console.log(readConfig);
+}, 1000);
